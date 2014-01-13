12e8df39536a2ba19d3c64a83d754b8871653ead
cactus <gerold.kathan@gmail.com>
2014-01-13 19:12:25 +0100

    initial commit

---
+++ data/data.php
@@ -0,0 +1,26 @@
+<?php
+// Make a MySQL Connection
+
+
+$type = htmlspecialchars($_GET["type"]);
+
+
+	mysql_connect("localhost", "root", "kakroot") or die(mysql_error());
+	mysql_select_db("kanban") or die(mysql_error());
+
+	mysql_query('SET CHARACTER SET utf8');
+	// Get all the data from the "example" table
+	$result = mysql_query("SELECT * FROM ".$type)
+	or die(mysql_error());  
+
+	$arr = array();
+
+	// keeps getting the next row until there are no more to get
+	while($row = mysql_fetch_object( $result )) {
+		$arr[]=$row;
+	} 
+	echo json_encode($arr);
+
+
+
+?>
