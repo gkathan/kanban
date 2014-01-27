3316a5aff566a2f697916d81cf4c4d321d4176ac
cactus <gerold.kathan@gmail.com>
2014-01-27 19:34:25 +0100

    first implementation of a simple auto-layout algorithm: layouts items per sublane which have same planDate in non-intersecting y-coordinates....

--- js/kanban.js
+++ js/kanban.js
@@ -2580,7 +2580,7 @@ function getItemByID(data,id){
 
 function getItemsBySublane(sublane){
 	var _sub = getSublaneByNameNEW(sublane);
-	if (_sub.children){
+	if (_sub && _sub.children){
 		return _sub.children;
 	} 
 }
@@ -2859,6 +2859,198 @@ function calculateQueueMetrics(){
 		
 	}
 }
+
+
+/* --------------------------------------------------------- auto layout section ------------------------------------------------------------*/
+
+function printItems(items){
+	for (var p=0;p<items.length;p++){
+		console.log("* items["+p+"]: id: "+items[p].id+" planDate: "+items[p].planDate);
+	}
+	
+}
+	
+/**
+ * takes an array of items and splits it up in segmented arrays by same planDates (current impl)
+ *
+ */	
+function segmentItemsByDate(items){
+	// first let's sort 
+	console.log("***before sort***");
+	printItems(items);
+	
+	
+	items.sort(function(a,b){return new Date(a.planDate).getTime()-new Date(b.planDate).getTime();})
+	
+	console.log("***after sort***");
+	printItems(items);
+	var _segments=new Array();
+	var e=0;
+	while(items.length>0){
+		console.log("in outer loop - e: "+e);
+		_segments[e] = new Array();
+			
+		for (var i=0 ;i<items.length;i++){
+			//console.log("in inner loop - i: "+i+  " i.planDate: "+items[i].planDate+"(e: "+e+") e.planDate: "+items[e].planDate);
+			if (items[0].planDate == items[i].planDate){
+				console.log("***e: "+e+" i: "+i+" same planDate found");
+				_segments[e].push(items[i]);
+				console.log("+++++ pushed in _segments["+e+"] id: "+items[i].id);
+			}
+			console.log("e="+e);
+		}
+		for(var j in _segments[e]){
+			printItems(items);
+			console.log("elements.length: "+items.length);
+			console.log("-> kicking out ["+j+"]: id: "+items[0].id);
+			console.log("e="+e);
+			items.splice(0,1);
+		}
+	e++;			
+	}
+	console.log("...and now ? e="+e);
+
+	
+	return _segments;
+}
+
+/**
+ * selects D3 SVG elements by item objects
+ * */
+function getElements(items){
+		var _ids = new Array();
+		for (var i in items){
+			_ids.push(items[i].id);
+		}
+		console.log(_ids);	
+		//var _ids=new Array(200,201,208,209)
+		var _elements = d3.select("#items").selectAll("g").filter(function(d){return _ids.indexOf(d.id)>=0;});
+		
+		return _elements;
+}
+
+
+function autoLayout(){
+	_lanes = getLanesNEW();
+	for (l in _lanes){
+		autoLayoutByLane(_lanes[l].name);
+	}
+	
+}
+
+function autoLayoutByLane(lane){
+	_sublanes = getSublanesNEW(lane);
+	for (s in _sublanes){
+		autoLayoutBySublane(_sublanes[s].name);
+	}
+}
+
+function autoLayoutBySublane(sublane){
+	var _items = getItemsBySublane(sublane);
+	var _segments = segmentItemsByDate(_items);
+	
+	for (var s in _segments){
+		layout(getElements(_segments[s]));
+	}
+}
+
+/**
+ * layout core algorithm 
+ * uses _getMetrics function to get proper info about transformed svg elements (getBBox is not sufficient...)
+ */
+function layout(elements){
+	var _total =0;
+	var _number = 0;
+	var _yList =new Array();
+
+	elements.each(function (d){
+		var _m =get_metrics(d3.select(this).node());
+		_total+=_m.height
+		console.log("h: "+_m.height+" t:"+_total+" y: "+_m.y);
+		_yList.push(_m.y);
+		_number++;
+	})
+
+	//var _ids = d3.select("#items").selectAll("g")[0];
+	var _min = d3.min(_yList);
+	var _height = _total/_number;
+	console.log("top y: "+_min);
+	console.log("number elements: "+_number);
+	console.log("_total height: "+_total);
+
+	var i=0;
+	var _space =0;
+	elements.each(function (d){
+		d3.select(this).attr("transform",function(d){return "translate(0,"+(_min+i-get_metrics(d3.select(this).node()).y)+")";});
+		i+=get_metrics(d3.select(this).node()).height+_space;
+	})
+}
+
+/**
+http://stackoverflow.com/questions/13046811/how-to-determine-size-of-raphael-object-after-scaling-rotating-it/13111598#13111598
+*/
+function get_metrics(el) {
+    function pointToLineDist(A, B, P) {
+        var nL = Math.sqrt((B.x - A.x) * (B.x - A.x) + (B.y - A.y) * (B.y - A.y));
+        return Math.abs((P.x - A.x) * (B.y - A.y) - (P.y - A.y) * (B.x - A.x)) / nL;
+    }
+
+    function dist(point1, point2) {
+        var xs = 0,
+            ys = 0;
+        xs = point2.x - point1.x;
+        xs = xs * xs;
+        ys = point2.y - point1.y;
+        ys = ys * ys;
+        return Math.sqrt(xs + ys);
+    }
+    var b = el.getBBox(),
+        objDOM = el,
+        svgDOM = objDOM.ownerSVGElement;
+    // Get the local to global matrix
+    var matrix = svgDOM.getTransformToElement(objDOM).inverse(),
+        oldp = [[b.x, b.y], [b.x + b.width, b.y], [b.x + b.width, b.y + b.height], [b.x, b.y + b.height]],
+        pt, newp = [],
+        obj = {},
+        i, pos = Number.POSITIVE_INFINITY,
+        neg = Number.NEGATIVE_INFINITY,
+        minX = pos,
+        minY = pos,
+        maxX = neg,
+        maxY = neg;
+
+    for (i = 0; i < 4; i++) {
+        pt = svgDOM.createSVGPoint();
+        pt.x = oldp[i][0];
+        pt.y = oldp[i][1];
+        newp[i] = pt.matrixTransform(matrix);
+        if (newp[i].x < minX) minX = newp[i].x;
+        if (newp[i].y < minY) minY = newp[i].y;
+        if (newp[i].x > maxX) maxX = newp[i].x;
+        if (newp[i].y > maxY) maxY = newp[i].y;
+    }
+    // The next refers to the transformed object itself, not bbox
+    // newp[0] - newp[3] are the transformed object's corner
+    // points in clockwise order starting from top left corner
+    obj.newp = newp; // array of corner points
+    obj.width = pointToLineDist(newp[1], newp[2], newp[0]) || 0;
+    obj.height = pointToLineDist(newp[2], newp[3], newp[0]) || 0;
+    obj.toplen = dist(newp[0], newp[1]);
+    obj.rightlen = dist(newp[1], newp[2]);
+    obj.bottomlen = dist(newp[2], newp[3]);
+    obj.leftlen = dist(newp[3], newp[0]);
+    // The next refers to the transformed object's bounding box
+    obj.x = minX;
+    obj.y = minY;
+    obj.x2 = maxX;
+    obj.y2 = maxY;
+    obj.width = maxX - minX;
+    obj.height = maxY - minY;
+    return obj;
+}
+
+
+
 var PACKAGE_VERSION="20140105_1132";
 var PACKAGE_VERSION="20140105_1225";
 var PACKAGE_VERSION="20140107_1602";
