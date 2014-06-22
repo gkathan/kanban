5f8941f80f8d77406d424c1187c5ff87c6eeffec
cactus <gerold.kathan@gmail.com>
2014-06-22 10:01:05 +0200

    adaptiions

--- js/kanban_config.js
+++ js/kanban_config.js
@@ -3,13 +3,16 @@ var host = window.location.host;
 
 var TRANSCODE_URL;
 var MONGO_GATEWAY_URL;
+
+var V1_PROD_URL = "http://v1.bwinparty.corp/V1-Production/";
+
 var V1_DATA_URL = "http://knbnprxy.ea.bwinparty.corp/rest/epics";
 //var V1_DATA_URL = "data/json/epics.json";
 
 var DS ="MONGO";
 //var DS ="MYSQL";
 
-
+var COLOR_BPTY="#174D75";
 
 if (host=="portfolio.ea.bwinparty.corp") {
 	MONGO_GATEWAY_URL = "http://portfolio.ea.bwinparty.corp/nodejs/";
@@ -35,4 +38,82 @@ function dataSourceFor(collection){
 	}
 }
 
+
+/** tests all needed connections
+ */
+function checkServices(){
+		var check = $.get( MONGO_GATEWAY_URL+"targets", function() {
+	  console.log("[DEBUG] checkServices()....");
+	})
+	  .done(function() {
+		console.log( " success" );
+	  })
+	  .fail(function() {
+		 $('.top-left').notify({
+				message: { html: "<span class=\"glyphicon glyphicon-fire\"></span><span style=\"font-size:10px;font-weight:bold\"> kanban.checkServices() says:</span> <br/><div style=\"font-size:10px;font-weight:normal;margin-left:20px\">* nodeJS datagateway ("+MONGO_GATEWAY_URL+") offline...<br>* please contact <a href=\"mailto:gerold.kathan@bwinparty.com?Subject=[kanban issue]#nodeJS datagateway offline\" target=\"_top\">[your corpkanban support team]</a> for assistance</div>" },
+				fadeOut: {enabled:false},
+				type: "danger"
+			  }).show(); // for the ones that aren't closable and don't fade out there is a .hide() function.
+				  });
+}
+
+function initShortcuts(){
+	Mousetrap.bind(['v'], function(e) {
+		console.log("redirect to v1sync");
+		window.location.href="v1sync.php";
+		return false;
+	});
+
+	Mousetrap.bind(['a i'], function(e) {
+		console.log("redirect to admin initiatives");
+		window.location.href="admin.php?type=initiatives";
+		return false;
+	});
+
+	Mousetrap.bind(['a t'], function(e) {
+		console.log("redirect to admin targets");
+		window.location.href="admin.php?type=targets";
+		return false;
+	});
+
+	Mousetrap.bind(['a m'], function(e) {
+		console.log("redirect to admin metrics");
+		window.location.href="admin.php?type=metrics";
+		return false;
+	});
+	
+		Mousetrap.bind(['a l'], function(e) {
+		console.log("redirect to admin lanetext");
+		window.location.href="admin.php?type=lanetext";
+		return false;
+	});
+
+
+
+	Mousetrap.bind(['k'], function(e) {
+		console.log("redirect to kanban");
+		window.location.href="kanban.php";
+		return false;
+	});
+	
+
+	Mousetrap.bind(['m'], function(e) {
+		console.log("open menus");
+		$('#kanban_menu').trigger('click');
+		return false;
+	});
+	
+	Mousetrap.bind(['e p'], function(e) {
+		console.log("export pdf");
+		$('#save_as_pdf').trigger('click');
+		return false;
+	});
+	
+	Mousetrap.bind(['e g'], function(e) {
+		console.log("export png");
+		$('#save_as_png').trigger('click');
+		return false;
+	});
+
+}
 
